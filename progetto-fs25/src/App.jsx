9f6693e1b81712e546b21a1f15f6d9f79ad9466d
Manfredi <manfredi.marrone@hotmail.it>
2024-05-16 16:06:33 +0200

    ripasso react

--- progetto-fs25/src/App.jsx
+++ progetto-fs25/src/App.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import ControlledForm from "./components/ControlledForm";
-
+import Title from "./components/Title";
 
 function App() {
+  const [name, setName] = useState("Come mi chiamo?");
+  const [userName, setUserName] = useState("");
+  
+
+  function handleChange(e) {
+    setUserName(e.target.value);
+  }
+
+  function handleClick() {
+    setName(userName);
+  }
+
+  
+
   return (
     <>
-      <h1 className="title">Benvenuto nel mio progetto</h1>
-    <ControlledForm/>
+      <h1>Benvenuti!</h1>
+      <Title/>
+      <input type="text" onChange={handleChange} />
+      <button onClick={() => handleClick("Paperino")}>Change Name</button>
+     
     </>
   );
 }
--- progetto-fs25/src/components/Title.jsx
+++ progetto-fs25/src/components/Title.jsx
@@ -1,15 +1,31 @@
-import { useContext } from "react";
-import LanguageContext from "../LanguageContext";
-import Card from "./Card";
+import { useEffect, useState } from "react";
 
-function Title() {
-  const websiteLang = useContext(LanguageContext);
-  return (
-    <>
-      <h2>La lingua è : {websiteLang}</h2>
-      <Card/>
-    </>
-  );
+function Title(){
+    const [todos, setTodos] = useState();
+
+    async function getData() {
+        try {
+          const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+          const data = await res.json();
+          console.log(data);
+          setTodos(data);
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+    
+      useEffect(() => {
+        getData();
+      }, []);
+    
+    return (
+        <div>
+        {todos ? todos.map((item) => {
+          return <p key={item.id + item.title}>{item.title} <b>{item.completed.toString()}</b></p>
+        }) : <h3>Caricamento...</h3>}
+      </div>
+    )
 }
 
 export default Title;
+
