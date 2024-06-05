e372dc3d0367b5f315de69031f5b19ccf3a6424d
Manfredi <manfredi.marrone@hotmail.it>
2024-06-05 10:06:19 +0200

    added fetch and local data

--- node-projectb/my-module.mjs
+++ node-projectb/my-module.mjs
@@ -18,3 +18,31 @@ export function total(){
     const sum = numArr.reduce((acc, curr) => acc + curr, 0)
     return sum
 }
+
+export const users = [
+    {
+      id: 1,
+      username: 'user1',
+      password: 'password1'
+    },
+    {
+      id: 2,
+      username: 'user2',
+      password: 'password2'
+    },
+    {
+      id: 3,
+      username: 'user3',
+      password: 'password3'
+    },
+    {
+      id: 4,
+      username: 'user4',
+      password: 'password4'
+    },
+    {
+      id: 5,
+      username: 'user5',
+      password: 'password5'
+    }
+  ];
--- node-projectb/server.mjs
+++ node-projectb/server.mjs
@@ -2,6 +2,7 @@ import os from 'os'
 import fs from 'fs'
 import http from 'http'
 const port = 3000
+import { users } from './my-module.mjs'
 
 /* fs.mkdir('assets', (err, data) => {
     if (err) {
@@ -51,24 +52,42 @@ const port = 3000
 }) */
 
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     console.log(req.method, req.url);
     if (req.url === '/') {
         res.setHeader('Content-Type', 'text/html')
         fs.readFile('./assets/test.txt', 'utf8', (err, data) => {
             if (err) {
                 console.log(err);
+                res.statusCode = 500
                 return
             }
             res.write(`<h1>${data}</h1>`)
             res.statusCode = 200
             res.end()
         })
-    } else if (req.url === '/contatti') {
-        res.setHeader('Content-Type', 'text/html')
-        res.write(`<h1>Pagina contatti</h1>`)
+    } else if (req.url === '/utenti') {
+        res.setHeader('Content-Type', 'application/json')
+        res.write(JSON.stringify(users))
         res.statusCode = 200
         res.end()
+
+    } else if (req.url === '/prodotti') {
+        try {
+            const response = await fetch('https://fakestoreapi.com/products')
+            const prods = await response.json()
+
+            const men = prods.filter(item => item.category === "men's clothing")
+
+            res.setHeader('Content-Type', 'application/json')
+            res.write(JSON.stringify(men))
+            res.statusCode = 200
+            res.end()
+        } catch (error) {
+            console.error(error.message);
+            res.statusCode = 500
+            res.end()
+        }
 
     } else {
         res.setHeader('Content-Type', 'text/html')
